e9646581827592bcb231e3f3fb58c00a875a1075
Anonymer Eintrag <tsche001@pc12-114.intern.mi.hs-rm.de>
2013-05-14 15:06:18 +0200

    Perspective Transformation added

--- blatt04/pointViewerPerspective.py
+++ blatt04/pointViewerPerspective.py
@@ -2,8 +2,8 @@ from Tkinter import *
 from Canvas import *
 import sys
 import random
-from math import *
-import numpy as np
+import math
+import numpy
 
 WIDTH  = 400 # width of canvas
 HEIGHT = 400 # height of canvas
@@ -21,44 +21,46 @@ def quit(root=None):
     root._root().quit()
     root._root().destroy()
 
+def perspectiveTransform(point, radius):
+    angle = math.radians(30)
+    cot = math.cos(angle) / math.sin(angle)
+    aspect = WIDTH/float(HEIGHT)
+    near = 1
+    far = 3
+    
+    a = numpy.matrix([[cot / aspect, 0, 0, 0],
+                      [0, cot, 0, 0],
+                      [0, 0, -(far + near) / (far - near), -2 * far * near / (far - near)],
+                      [0, 0, -1, 0]])
+    b = numpy.matrix([[x] for x in point])
+    res = (a * b).tolist()
+    return [res[0][0], res[1][0], res[2][0], res[3][0]]
+
+def lookAtTransform(point, radius):
+    a = numpy.matrix([[1, 0, 0, 0],
+                      [0, 1, 0, 0],
+                      [0, 0, 1, -2*radius],
+                      [0, 0, 0, 1]])
+    b = numpy.matrix([[x] for x in point])
+    res = (a * b).tolist()
+    return [res[0][0], res[1][0], res[2][0], res[3][0]]
+
+
 def draw():
     """ draw points """
-    global canonViewVolume
-
-    # perspective transformation (multiplicate with Perspective Transformation Matrix)
+    global transformedPoints, center, boundingBox
     
-    # calculate radius r
-    radius = sqrt(sum(map(lambda x: x*x, map(lambda x,y: x-y, center, boundingBox[0])))) 
-    print radius
+    radius = math.sqrt(sum(map(lambda x: x*x, map(lambda x,y: x-y, center, boundingBox[0])))) 
 
-    # init camera-coords with e=(0,0,d=2r), c=(0,0,0), up=(0,1,0)
-    e = [0,0,2*radius]
-    c = [0,0,0]
-    up = [0,1,0]
+    modelViewPoints = map(lambda x: lookAtTransform(x, radius), transformedPoints)
+    canonViewPoints = map(lambda x: perspectiveTransform(x, radius), modelViewPoints)
 
-    f = [1.0,0,0]#map(lambda x: x/abs(2*radius), map(lambda x,y: x-y, c, e))
-    s = [0,0,1.0] #per hand gerechnet
-    u = [0,-1.0,0] #per hand gerechnet
+    perspectivelyDivided = [[point[0]/point[3], point[1]/point[3], point[1]/point[3], 1.] for point in canonViewPoints]
 
-    print f,s,u
+    points2d = [point[:2] for point in perspectivelyDivided]
 
-    
-    # modelview-transformation (multiplicate with LookAt-Matrix)
-    modelViewPoints = [[s[0]*p[0], u[1]*p[1], -f[2]*p[2]]  for p in dataPoints]  
-    
-    near = radius
-    far = 3*radius
-    angle = radians(30)
-    cot = cos(angle)/sin(angle)
-    aspect = WIDTH/float(HEIGHT)
-
-    canonViewVolume = [[(cot/aspect * p[0])/-p[2], (cot * p[1])/-p[2], ((-(far+near)/(far-near) * p[2]) + (-(2*far*near)/(far-near)))/-p[2]] for p in modelViewPoints]
-
-    points2d = [point[:2] for point in canonViewVolume]
+    # clip z-coordinate
     viewport = [[(point[0] + 1) * WIDTH/2.0, (1 - point[1]) * HEIGHT/2.0] for point in points2d]
-
-    # viewport-transformation
-    # viewport = ?
     
     for e in viewport:
         x, y = e[0], e[1]
@@ -68,26 +70,35 @@ def draw():
 
 def rotYp():
     """ rotate counterclockwise around y axis """
-    angle = math.radians(20)
-    rotate(angle)
+    global transformedPoints
+    
+    angle = math.pi * 20. / 180
+    transformedPoints = map(lambda x: rotate(x, angle), transformedPoints)
 
     can.delete(*pointList)
     draw()
 
 def rotYn():
     """ rotate clockwise around y axis """
-    angle = -math.radians(20)
-    rotate(angle)
+    global transformedPoints
     
+    angle = -math.pi * 20. / 180
+    transformedPoints = map(lambda x: rotate(x, angle), transformedPoints)
+
     can.delete(*pointList)
     draw()
 
-def rotate(angle):
-    global canonViewVolume
-    cos = cos(angle)
-    sin = sin(angle)
+def rotate(point, angle):
+    cos = math.cos(angle)
+    sin = math.sin(angle)
 
-    canonViewVolume = [[cos*point[0] + sin*point[2], point[1], -sin*point[0] + cos*point[2]] for point in canonViewVolume]
+    a = numpy.matrix([[cos, 0, -sin, 0],
+                      [0, 1, 0, 0],
+                      [sin, 0, cos, 0],
+                      [0, 0, 0, 1]])
+    b = numpy.matrix([[x] for x in point])
+    res = (a * b).tolist()
+    return [res[0][0], res[1][0], res[2][0], res[3][0]]
 
     
 if __name__ == "__main__":
@@ -100,7 +111,7 @@ if __name__ == "__main__":
     mw = Tk()
 
     # read data
-    dataPoints = [map(float, line.split()) for line in file('data/cow_points.raw')]
+    dataPoints = [map(float, line.split()+[1.]) for line in file('data/cow_points.raw')]
 
     # bounding box
     boundingBox = [map(min, zip(*dataPoints)), map(max, zip(*dataPoints))]
@@ -112,7 +123,7 @@ if __name__ == "__main__":
     sideScale = 2.0 / max([(coords[1] - coords[0]) for coords in zip(*boundingBox)])
 
     # move center of bounding box to origin and scale to a length of 2.0
-    #canonViewVolume = [[(point[0] - center[0])*sideScale, (point[1] - center[1])*sideScale, (point[2] - center[2])*sideScale] for point in dataPoints]
+    transformedPoints = [[(point[0] - center[0])*sideScale, (point[1] - center[1])*sideScale, (point[2] - center[2])*sideScale, 1.*sideScale] for point in dataPoints]
     
     # create and position canvas and buttons
     cFr = Frame(mw, width=WIDTH, height=HEIGHT, relief="sunken", bd=1)
